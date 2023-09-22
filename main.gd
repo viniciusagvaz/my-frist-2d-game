4869983bf9c7cac412d77afbb1ced3b8c4425edb
viniciusagvaz <agvazvinicius@gmail.com>
2023-09-22 18:02:37 -0300

    [updating] game_over

--- main.gd
+++ main.gd
@@ -40,10 +40,8 @@ func _on_mob_timer_timeout():
 	# Choose the velocity for the mob.
 	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
 	mob.linear_velocity = velocity.rotated(direction)
-
+	
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
 	
-func _ready():
-	new_game()
-
+	$HUD.update_score(score)
